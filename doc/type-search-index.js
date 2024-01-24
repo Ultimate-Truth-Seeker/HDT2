0b7fac22b3ea99c7ff95815fb7b394cd39ac5bae
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-01-23 20:24:50 -0600

    documentación javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calculadora"},{"p":"<Unnamed>","l":"CustomStack"},{"p":"<Unnamed>","l":"ICalculadora"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Node"}];updateSearchResults();
